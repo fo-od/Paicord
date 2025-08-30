8a6e53a8c2360400093778872e40d54991b6550c
Lakhan Lothiyi <llsc12.dev@gmail.com>
2025-08-31 00:19:48 +0100

    Initial Commit

---
+++ PaiCord/PaiCordApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaiCordApp.swift
+//  PaiCord
+//
+//  Created by Lakhan Lothiyi on 31/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PaiCordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
