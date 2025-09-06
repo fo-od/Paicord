73c0e281d91c1eb319e2fdb8d899362d9cabf748
Lakhan Lothiyi <llsc12.dev@gmail.com>
2025-09-06 16:13:57 +0100

    login stuff

---
+++ Paicord/Utilities/Extensions/String+LocalizedError.swift
@@ -0,0 +1,14 @@
+//
+//  String+LocalizedError.swift
+//  Paicord
+//
+//  Created by Lakhan Lothiyi on 06/09/2025.
+//
+
+import Foundation
+
+extension String: LocalizedError {
+  public var errorDescription: String? {
+    self
+  }
+}
